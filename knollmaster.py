8cb1de59d58feb7633f6faef22d60646b31e5330
ChadTech <chadstearns1@gmail.com>
2014-05-14 16:40:01 -0700

    Turned everything into functions and classes

--- knollmaster.py
+++ knollmaster.py
@@ -11,34 +11,53 @@ pygame.midi.init()
 clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((1000,750),pygame.RESIZABLE)
-pygame.display.set_caption("Chadtech v5.00 : Knoll Master",)
+pygame.display.set_caption("Chadtech v5.00 : Knollmaster",)
 
 knollTable = pygame.image.load('tableB.png').convert()
 
+class knollZone:
+	def __init__(self,image,xSize,ySize,xPos,yPos):
+		self.image=image
+		self.xSize=xSize
+		self.ySize=ySize
+		self.xPos=xPos
+		self.yPos=yPos
+		self.angle=angle
+
+class itemType:
+	def __init__(self,image,xSize,ySize):
+		self.image = image
+		self.xSize=xSize
+		self.ySize=ySize
+
+class itemInstance:
+	def __init__(self,itemType,xPos,yPos,angle):
+		self.itemType=itemType
+		self.xPos=xPos
+		self.yPos=yPos
+		self.angle=angle
+
+def blitter(what):
+	global screen
+	placee=pygame.transform.rotate(what.itemType.image,what.angle).convert()
+	placeeX,placeeY = placee.get_size()
+	screen.blit(placee,[what.xPos-(placeeX/2),what.yPos-(placeeY/2)])
+
 os.chdir(os.path.abspath('items'))
 
-drill0=pygame.image.load('drill0.png').convert()
-drill0.set_colorkey((255,255,255,255))
+itemMapper = {}
 
-calculator0=pygame.image.load('calculator0.png').convert()
-calculator0.set_colorkey((255,255,255,255))
+for item in os.listdir(os.getcwd()):
+	xSize, ySize = pygame.image.load(item).get_size()
+	itemMapper[item[:len(item)-4]] = itemType(pygame.transform.scale(pygame.image.load(item),(xSize*2,ySize*2)).convert(),xSize*2,ySize*2)
+	itemMapper[item[:len(item)-4]].image.set_colorkey((255,255,255,255))
 
-knife0=pygame.image.load('knife0.png').convert()
-knife0.set_colorkey((255,255,255,255))
-
-pokeball0=pygame.image.load('pokeball0.png').convert()
-pokeball0.set_colorkey((255,255,255,255))
-
-clamp0=pygame.image.load('clamp0.png').convert()
-clamp0.set_colorkey((255,255,255,255))
-
-glasses0=pygame.image.load('glasses0.png').convert()
-glasses0.set_colorkey((255,255,255,255))
-
-title=pygame.image.load('chadtechnknollmastertitle.png').convert()
-title.set_colorkey((255,255,255,255))
+firstDrill = itemInstance(itemMapper['drill0'],400,400,45)
 
 os.chdir(os.path.dirname(os.getcwd()))
+
+title=pygame.image.load('chadtechknollmastertitle.png').convert()
+title.set_colorkey((255,255,255,255))
 
 mainLoop = True
 quit = False
@@ -51,25 +70,10 @@ while mainLoop and not quit:
 	screen.blit(pygame.transform.scale(title,(564,124)),[225,5])
 	angle+=1
 
-	drillX, drillY = pygame.transform.rotate(pygame.transform.scale(drill0,(130,130)),angle).get_size()
-	screen.blit(pygame.transform.rotate(pygame.transform.scale(drill0,(130,130)),angle),[200-(drillX/2),300-(drillY/2)])
+	firstDrill.angle+=1
 
-	calcX, calcY = pygame.transform.rotate(pygame.transform.scale(calculator0,(48,88)),angle*2.12).get_size()
-	screen.blit(pygame.transform.rotate(pygame.transform.scale(calculator0,(48,88)),angle*2.12),[400-(calcX/2),400-(calcY/2)])
+	blitter(firstDrill)
 
-	knifeX, knifeY = pygame.transform.rotate(pygame.transform.scale(knife0,(16,80)),angle*7.13).get_size()
-	screen.blit(pygame.transform.rotate(pygame.transform.scale(knife0,(16,80)),angle*7.13),[800-(knifeX/2),400-(knifeY/2)])
-
-	pokeballX, pokeballY = pygame.transform.rotate(pygame.transform.scale(pokeball0,(42,42)),angle*4.53).get_size()
-	screen.blit(pygame.transform.rotate(pygame.transform.scale(pokeball0,(42,42)),angle*4.53),[600-(pokeballX/2),500-(pokeballY/2)])
-
-	clampX, clampY = pygame.transform.rotate(pygame.transform.scale(clamp0,(58,206)),angle*0.73).get_size()
-	screen.blit(pygame.transform.rotate(pygame.transform.scale(clamp0,(58,206)),angle*0.73),[600-(clampX/2),250-(clampY/2)])
-
-	glassesX, glassesY = pygame.transform.rotate(pygame.transform.scale(glasses0,(96,30)),angle*1.73).get_size()
-	screen.blit(pygame.transform.rotate(pygame.transform.scale(glasses0,(96,30)),angle*1.73),[150-(glassesX/2),470-(glassesY/2)])
-
-	#screen.blit(pygame.transform.rotate(theWorld.background,-lander.angle),[(800-worldX),400-worldY])
 	for event in pygame.event.get():
 		#if event.type == pygame.KEYDOWN:
 		#	if event.key == pygame.K_a:
