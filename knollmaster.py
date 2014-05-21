ee2f9670daf17d5eb0f8ed6eb6d68ed0d7619a52
ChadTech <chadstearns1@gmail.com>
2014-05-20 19:43:14 -0700

    Did a lot of code. Game now groups similar items, and blinks the group during scoring

--- knollmaster.py
+++ knollmaster.py
@@ -39,6 +39,27 @@ class knollZone:
 		self.xSize=xSize
 		self.ySize=ySize
 
+class region:
+	def __init__(self,itemType):
+		self.lBou=''
+		self.rBou=''
+		self.tBou=''
+		self.bBou=''
+		self.itemType=itemType
+		self.exists=False
+	#def getBoundaries(self):
+	#	global itemsOnSurface
+	#	for item in itemsOnSurface:
+	#		if type(self.lBou)==str or (type(self.lBou)==int and (item.xPos-(item.itemType.xSize/2))<self.lBou):
+	#			self.lBou=item.xPos-(item.itemType.xSize/2)
+	#		if type(self.rBou)==str or (type(self.rBou)==int and self.rBou<(item.xPos+(item.itemType.xSize/2))):
+	#			self.rBou=item.xPos+(item.itemType.xSize/2)
+	#		if type(self.tBou)==str or (type(self.tBou)==int and self.tBou<(item.yPos+(item.itemType.ySize/2))):
+	#			self.tBou=item.yPos+(item.itemType.ySize/2)
+	#		if type(self.bBou)==str or (type(self.bBou)==int and (item.yPos-(item.itemType.ySize/2))<self.bBou):
+	#			self.bBou=item.yPos-(item.itemType.ySize/2)
+
+
 table0 = knollZone('table.png')
 surface=table0
 
@@ -50,6 +71,9 @@ botBou=topBou+surface.ySize
 
 class itemType:
 	def __init__(self,image):
+
+		self.name=image[:len(image)-4]
+
 		image=pygame.image.load(image).convert()
 		xSize,ySize=image.get_size()
 		xSize,ySize=xSize*2,ySize*2
@@ -62,7 +86,9 @@ class itemType:
 
 class itemInstance:
 	def __init__(self,itemType,xPos,yPos,angle):
+
 		self.itemType=itemType
+		self.name=itemType.name
 		self.xPos=xPos
 		self.yPos=yPos
 		self.angle=angle
@@ -85,7 +111,6 @@ def supercoolText(inputString,where):
 	screen.blit(ctCambridge.render(inputString,False,(24,13,170)),[whereX+4,whereY-4])
 	screen.blit(ctCambridge.render(inputString,False,(24,13,170)),[whereX-4,whereY-4])
 
-
 	screen.blit(ctCambridge.render(inputString,False,(63,72,204)),[whereX+4,whereY])
 	screen.blit(ctCambridge.render(inputString,False,(63,72,204)),[whereX-4,whereY])
 	screen.blit(ctCambridge.render(inputString,False,(63,72,204)),[whereX,whereY-4])
@@ -98,10 +123,12 @@ def supercoolText(inputString,where):
 ################################
 os.chdir(os.path.abspath('items'))
 itemMapper = {}
+itemMappee = {}
 listOfItems = []
 for item in os.listdir(os.getcwd()):
 	xSize, ySize = pygame.image.load(item).get_size()
 	itemMapper[item[:len(item)-4]] = itemType(item)
+	itemMapper[itemType(item)]=item[:len(item)-4]
 	listOfItems.append(item[:len(item)-4])
 os.chdir(os.path.dirname(os.getcwd()))
 
@@ -113,6 +140,9 @@ titleX,titleY=title.get_size()
 titleX,titleY=titleX*4,titleY*4
 title = pygame.transform.scale(title,(titleX,titleY))
 title.set_colorkey((255,255,255,255))
+
+
+
 
 
 ################################
@@ -134,6 +164,12 @@ scoreTrigger = False
 itemsOnSurface=[]
 
 timer = 500
+groupBlink = 0 
+
+################################
+##### Load items in level ######
+################################
+
 
 itemsOnSurface.append(itemInstance(itemMapper['drill0'],leftBou+itemMapper['drill0'].xSize,botBou-itemMapper['drill0'].ySize,60))
 itemsOnSurface.append(itemInstance(itemMapper['drill0'],itemMapper['drill0'].xSize+leftBou,topBou+itemMapper['drill0'].ySize,66))
@@ -141,8 +177,17 @@ itemsOnSurface.append(itemInstance(itemMapper['drill0'],itemMapper['drill0'].xSi
 itemsOnSurface.append(itemInstance(itemMapper['pokeball0'],itemMapper['pokeball0'].xSize+300+leftBou,topBou+itemMapper['pokeball0'].ySize+27,166))
 itemsOnSurface.append(itemInstance(itemMapper['pokeball0'],itemMapper['pokeball0'].xSize+440+leftBou,topBou+itemMapper['pokeball0'].ySize+127,180))
 
-itemsOnSurface.append(itemInstance(itemMapper['clamp0'],itemMapper['clamp0'].xSize+leftBou+700,topBou+itemMapper['clamp0'].ySize+200,315))
+itemsOnSurface.append(itemInstance(itemMapper['clamp0'],itemMapper['clamp0'].xSize+leftBou+500,topBou+itemMapper['clamp0'].ySize+200,315))
 
+################################
+### Make region for itemtypes ##
+################################
+
+itemRegions = {}
+itemTypeChecker =[]
+
+for item in itemsOnSurface:
+	itemRegions[item.itemType.name]=('','','','')
 
 
 while mainLoop and not quit:
@@ -162,6 +207,12 @@ while mainLoop and not quit:
 	supercoolText('Time:'+str(timer),(20,93+(8*math.sin(bob/4.))))
 	if scoreTrigger:
 		supercoolText('Score:'+str(angleAve)[:6]+'%',(20,166+(8*math.sin(bob/4.))))
+		if groupBlink<80:
+			if ((groupBlink/5)%2) == 1:
+				for item in itemsOnSurface:
+					t,r,b,l = itemRegions[item.itemType.name]
+					pygame.draw.rect(screen, (255,0,0),(l,t,r-l,b-t),2)
+			groupBlink+=1
 	bob+=1
 
 	if not timer<1:
@@ -177,8 +228,34 @@ while mainLoop and not quit:
 			angleAve=100.*angleAve
 
 			penaltyCou=0
+			itemZones = []
+			
 			for item in itemsOnSurface:
+				tBou,rBou,bBou,lBou = itemRegions[item.itemType.name]
+				if type(tBou)==str or (type(tBou)==int and (item.yPos-(item.itemType.ySize/2))<tBou):
+					tBou=item.yPos-(item.itemType.ySize/2)
+				if type(rBou)==str or (type(rBou)==int and rBou<(item.xPos+(item.itemType.xSize/2))):
+					rBou=item.xPos+(item.itemType.xSize/2)
+				if type(bBou)==str or (type(bBou)==int and bBou<(item.yPos+(item.itemType.ySize/2))):
+					bBou=item.yPos+(item.itemType.ySize/2)
+				if type(lBou)==str or (type(lBou)==int and (item.xPos-(item.itemType.xSize/2))<lBou):
+					lBou=item.xPos-(item.itemType.xSize/2)
 
+				itemRegions[item.itemType.name]=(tBou,rBou,bBou,lBou)
+
+				print item.itemType.name, itemRegions[item.itemType.name]
+
+	#def getBoundaries(self):
+	#	global itemsOnSurface
+	#	for item in itemsOnSurface:
+	#		if type(self.lBou)==str or (type(self.lBou)==int and (item.xPos-(item.itemType.xSize/2))<self.lBou):
+	#			self.lBou=item.xPos-(item.itemType.xSize/2)
+	#		if type(self.rBou)==str or (type(self.rBou)==int and self.rBou<(item.xPos+(item.itemType.xSize/2))):
+	#			self.rBou=item.xPos+(item.itemType.xSize/2)
+	#		if type(self.tBou)==str or (type(self.tBou)==int and self.tBou<(item.yPos+(item.itemType.ySize/2))):
+	#			self.tBou=item.yPos+(item.itemType.ySize/2)
+	#		if type(self.bBou)==str or (type(self.bBou)==int and (item.yPos-(item.itemType.ySize/2))<self.bBou):
+	#			self.bBou=item.yPos-(item.itemType.ySize/2)
 
 			scoreTrigger = True
 
